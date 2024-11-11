d46f04e1c15775ad1b17bbec8ab8a2972399309a
salikhova regina <yanadera@mail.ru>
2024-11-11 18:07:25 +0400

    end of the working day

--- src/components/Shop/script.js
+++ src/components/Shop/script.js
@@ -2,42 +2,54 @@ import $ from 'jquery';
 import 'slick-slider'
 
 $(function() {
-    $('.slider--shop').slick({
-        arrow: true,
-        infinite: false,
-        initialSlide: 0,
-        rows:1,
-        slidesPerRow: 1,
-        centerMode: true,
-        responsive:
-        [
-            {
-                breakpoint: 1024,
-                settings: {
-                    rows:4,
-                    slidesPerRow: 3,
-                    centerMode: false,
-                }
-            },
-            {
-                breakpoint: 910,
-                settings: {
-                    rows:4,
-                    slidesPerRow: 2,
-                    centerMode: false,
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    centerMode: true,
-                    centerPadding: '0',
-                    rows: 1,
-                    slidesPerRow: 1,
-                }
-            }
-        ]
-    })
+        $('.slider--shop').slick({
+            arrows: true,
+            infinite: false,
+            slidesToShow:1.5,
+            slidesToScroll: 1,
+        })
+
+// $(function() {
+//     $('.slider--shop').slick({
+//         arrow: true,
+//         infinite: false,
+//         initialSlide: 0,
+//         // rows:1,
+//         // centerMode: true,
+//         slidesPerRow: 1,
+//         slidesToShow: 4,        // Показывать 4 слайда
+//         slidesToScroll: 1,      // Переключать 1 слайд за раз
+//         responsive:
+//         [
+//             {
+//                 breakpoint: 1024,
+//                 settings: {
+//                     rows:4,
+//                     slidesPerRow: 3,
+//                     centerMode: false,
+//                 }
+//             },
+//             {
+//                 breakpoint: 910,
+//                 settings: {
+//                     rows:4,
+//                     slidesPerRow: 2,
+//                     centerMode: false,
+//                 }
+//             },
+//             {
+//                 breakpoint: 768,
+//                 settings: {
+//                     centerMode: true,
+//                     centerPadding: '0',
+//                     rows: 1,
+//                     slidesPerRow: 1,
+//                     slidesToShow: 4,        // Показывать 4 слайда
+//                     slidesToScroll: 1,      // Переключать 1 слайд за раз
+//                 }
+//             }
+//         ]
+//     })
 
     $(function () {
         function updateArrowOpacity(currentSlide, slideCount) {
