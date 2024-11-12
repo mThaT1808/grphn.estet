180cc018c5ae8d45338a6ba78be4b0994dfbdb4f
salikhova regina <yanadera@mail.ru>
2024-11-12 15:57:15 +0400

    31626.shops fix slider

--- src/components/Shop/script.js
+++ src/components/Shop/script.js
@@ -1,79 +1,92 @@
 import $ from 'jquery';
 import 'slick-slider'
 
-$(function() {
-        $('.slider--shop').slick({
-            arrows: true,
+function checkWidth() {
+    const slider = $('.slider--shop');
+    if ($(window).width() < 768) {
+        slider.slick({
+            slidesToShow: 1.5,
+            rows: 1,
+            easing: "ease",
             infinite: false,
-            slidesToShow:1.5,
-            slidesToScroll: 1,
-        })
-
-// $(function() {
-//     $('.slider--shop').slick({
-//         arrow: true,
-//         infinite: false,
-//         initialSlide: 0,
-//         // rows:1,
-//         // centerMode: true,
-//         slidesPerRow: 1,
-//         slidesToShow: 4,        // Показывать 4 слайда
-//         slidesToScroll: 1,      // Переключать 1 слайд за раз
-//         responsive:
-//         [
-//             {
-//                 breakpoint: 1024,
-//                 settings: {
-//                     rows:4,
-//                     slidesPerRow: 3,
-//                     centerMode: false,
-//                 }
-//             },
-//             {
-//                 breakpoint: 910,
-//                 settings: {
-//                     rows:4,
-//                     slidesPerRow: 2,
-//                     centerMode: false,
-//                 }
-//             },
-//             {
-//                 breakpoint: 768,
-//                 settings: {
-//                     centerMode: true,
-//                     centerPadding: '0',
-//                     rows: 1,
-//                     slidesPerRow: 1,
-//                     slidesToShow: 4,        // Показывать 4 слайда
-//                     slidesToScroll: 1,      // Переключать 1 слайд за раз
-//                 }
-//             }
-//         ]
-//     })
-
-    $(function () {
-        function updateArrowOpacity(currentSlide, slideCount) {
-            // Сброс стилей стрелок
-            $('.slick-prev').css('opacity', '1');
-            $('.slick-next').css('opacity', '1');
-
-            // Установка opacity для левой стрелки, если это первый слайд
-            if (currentSlide === 0) {
-                $('.slick-prev').css('opacity', '0.2');
-            }
-
-            // Установка opacity для правой стрелки, если это последний слайд
-            if (currentSlide === slideCount - 1) {
-                $('.slick-next').css('opacity', '0.2');
-            }
-        }
-
-        // Обновление стрелок после смены слайда
-        $('.slider--shop').on('afterChange', function (event, slick, currentSlide) {
-            updateArrowOpacity(currentSlide, slick.slideCount);
         });
 
-        // Установка начального состояния стрелок при загрузке страницы
-        updateArrowOpacity(0, $('.slider--shop .slick-slide').length);
+        $('.arrow--shop-prev').on('click', function(e) {
+            e.preventDefault();
+            slider.slick('slickPrev');
+        });
+        $('.arrow--shop-next').on('click', function(e) {
+            e.preventDefault();
+            slider.slick('slickNext');
+        });
+
+    }else if ($(window).width() < 1366) {
+            slider.slick({
+                slidesToShow: 2,
+                rows: 4,
+                easing: "ease",
+                infinite: false,
+            });
+
+            $('.arrow--shop-prev').on('click', function(e) {
+                e.preventDefault();
+                slider.slick('slickPrev');
+            });
+            $('.arrow--shop-next').on('click', function(e) {
+                e.preventDefault();
+                slider.slick('slickNext');
+            });
+
+    } else {
+        slider.slick({
+            easing: "ease",
+            infinite: false,
+            slidesToShow: 4.5,
+            slidesToScroll: 1,
+            rows:1,
+        });
+
+        $('.arrow--shop-prev').on('click', function(e) {
+            e.preventDefault();
+            slider.slick('slickPrev');
+        });
+        $('.arrow--shop-next').on('click', function(e) {
+            e.preventDefault();
+            slider.slick('slickNext');
+        });
+    }
+}
+
+$(document).ready(function() {
+    checkWidth();
+});
+
+$(window).on('resize', function() {
+    checkWidth();
+});
+
+$(function () {
+    function updateArrowOpacity(currentSlide, slideCount) {
+        // Сброс стилей стрелок
+        $('.arrow--shop-prev').css('opacity', '1');
+        $('.arrow--shop-next').css('opacity', '1');
+
+        // Установка opacity для левой стрелки, если это первый слайд
+        if (currentSlide === 0) {
+            $('.arrow--shop-prev').css('opacity', '0.2');
+        }
+
+        // Установка opacity для правой стрелки, если это последний слайд
+        if (currentSlide === slideCount - 1) {
+            $('.arrow--shop-next').css('opacity', '0.2');
+        }
+    }
+
+    // Обновление стрелок после смены слайда
+    $('.slider--shop').on('afterChange', function (event, slick, currentSlide) {
+        updateArrowOpacity(currentSlide, slick.slideCount);
     });
-})
+
+    // Установка начального состояния стрелок при загрузке страницы
+    updateArrowOpacity(0, $('.slider--shop .slick-slide').length);
+});
