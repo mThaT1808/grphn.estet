be2533bab666a19862177b13effbafbfce260f2c
Dasha Savelyeva <ssavellievaa@yandex.ru>
2024-11-12 15:18:02 +0400

    31629.home.about initial slider again

--- src/components/About/script.js
+++ src/components/About/script.js
@@ -1,52 +1,58 @@
 import $ from 'jquery';
 import 'slick-slider';
 
-// function changeTemplate() {
-//     if ($(window).width() < 768) {
-//         console.log('перемещение в контейнер');
-//         $('.about .about__container .about__inner .about__main').prependTo('.about .about__container');
-//     } else {
-//         console.log('перемещение в inner');
-//         $('.about .about__container .about__main').prependTo('.about__inner');
-//     }
-// }
+function changeTemplate() {
+    if ($(window).width() < 768) {
+        console.log('перемещение в контейнер');
+        $('.about .about__container .about__inner .about__main').prependTo('.about .about__container');
+    } else {
+        console.log('перемещение в inner');
+        $('.about .about__container .about__main').prependTo('.about__inner');
+    }
+}
 
-// function checkWidth() {
-//     const slider = $('.about__inner');
-//     if ($(window).width() < 768) {
+function checkWidth() {
+    const slider = $('.about__inner');
+    if ($(window).width() < 768) {
 
-//         if (!slider.hasClass('slick-initialized')) {
-//             slider.slick({
-//                 arrows: false,
-//                 autoplay: true,
-//                 mobileFirst: true,
-//                 easing: "ease-in-out",
-//                 speed: 1000,
-//             });
+        if (!slider.hasClass('slick-initialized')) {
+            slider.slick({
+                arrows: false,
+                autoplay: true,
+                mobileFirst: true,
+                easing: "ease-in-out",
+                speed: 1000,
+            });
 
-//             $('.arrow--prev').on('click', function(e) {
-//                 e.preventDefault();
-//                 slider.slick('slickPrev');
-//             });
-//             $('.arrow--next').on('click', function(e) {
-//                 e.preventDefault();
-//                 slider.slick('slickNext');
-//             });
-//         }
-//     } else {
+            $('.arrow--prev').on('click', function(e) {
+                e.preventDefault();
+                slider.slick('slickPrev');
+            });
 
-//         if (slider.hasClass('slick-initialized')) {
-//             slider.slick('unslick');
-//         }
-//     }
-// }
+            $('.arrow--next').on('click', function(e) {
+                e.preventDefault();
+                slider.slick('slickNext');
+            });
 
-// $(document).ready(function() {
-//     changeTemplate();
-//     // checkWidth();
-// });
+            // slider.slick('slickRemove', 2, true);
+            slider.slick('setPosition');
+        }
+    } else {
 
-// $(window).on('resize', function() {
-//     changeTemplate();
-//     // checkWidth();
-// });
+        if (slider.hasClass('slick-initialized')) {
+            slider.slick('unslick');
+            // slider.slick('slickAdd','<div class="about__wrapper about__wrapper--empty"><picture class="about__image-box about__image-box--empty"><source srcset="/images/about/empty-tablet.webp" type="image/webp"><img class="about__image about__image--empty" src="/images/about/empty-tablet.jpg" alt=""></picture></div>', 2);
+            slider.slick('setPosition');
+        }
+    }
+}
+
+$(document).ready(function() {
+    changeTemplate();
+    checkWidth();
+});
+
+$(window).on('resize', function() {
+    changeTemplate();
+    checkWidth();
+});
