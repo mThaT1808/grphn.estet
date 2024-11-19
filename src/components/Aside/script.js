fc0ca42ff3e6f6e428685f5d9bd838c3ba6ef8bb
salikhova regina <yanadera@mail.ru>
2024-11-19 14:01:09 +0400

    31762.doors.aside style finish

--- src/components/Aside/script.js
+++ src/components/Aside/script.js
@@ -1,23 +1,6 @@
-// document.querySelectorAll('.filter__opener').forEach(button => {
-//     button.addEventListener('click', function() {
-//         const filterList = this.nextElementSibling; // Находим следующий элемент (список)
-
-//         // Закрываем другие списки
-//         document.querySelectorAll('.filter__list').forEach(list => {
-//             if (list !== filterList) {
-//                 list.classList.remove('active');
-//             }
-//         });
-
-//         // Переключаем активный класс у текущего списка
-//         filterList.classList.toggle('active');
-//     });
-// });
-
-
 document.querySelectorAll('.filter__opener').forEach(button => {
     button.addEventListener('click', function() {
-        const filterList = this.nextElementSibling;
+        const filterList = this.nextElementSibling; // Находим следующий элемент (список)
 
         // Закрываем другие списки
         document.querySelectorAll('.filter__list').forEach(list => {
