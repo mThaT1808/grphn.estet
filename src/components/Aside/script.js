42d3b4931d605308c0d0640ca1dc8b8b12a7d800
salikhova regina <yanadera@mail.ru>
2024-11-22 14:38:55 +0400

    31787.doors.aside.736 fix padding in buttons, color searchbar, moved the script

--- src/components/Aside/script.js
+++ src/components/Aside/script.js
@@ -22,3 +22,17 @@ document.querySelectorAll('.filter__opener').forEach(button => {
         }
     });
 });
+
+const collectionsButton = document.getElementById('collectionsButton');
+const allDoorsButton = document.getElementById('allDoorsButton');
+
+
+collectionsButton.addEventListener('click', () => {
+    collectionsButton.classList.remove('button--pink');
+    allDoorsButton.classList.add('button--pink');
+});
+
+allDoorsButton.addEventListener('click', () => {
+    allDoorsButton.classList.remove('button--pink');
+    collectionsButton.classList.add('button--pink');
+});
