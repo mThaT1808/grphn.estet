13f1717544cb1d3ab8261ec95078be048da18511
Dasha Savelyeva <ssavellievaa@yandex.ru>
2024-11-28 13:14:22 +0400

    31785.doors.card.790

--- src/components/Card/script.js
+++ src/components/Card/script.js
@@ -1,10 +1,10 @@
 const showMore = document.querySelector('.button--more');
-const doorLength = document.querySelectorAll('.door').length - 1;
+const doorLength = document.querySelectorAll('.door').length;
 const cardPagination = document.querySelector('.card__pagination');
 let items = 9;
 
 showMore.addEventListener('click', () => {
-	items += 4;
+	items += 5;
 	const array = Array.from(document.querySelector('.card__inner').children);
 	const visItems = array.slice(0, items);
 
