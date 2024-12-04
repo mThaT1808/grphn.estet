6f9937859f3c946c1bbc9c1a1a00b7c8d9d89b79
Артём <prubv@yandex.ru>
2024-12-04 16:40:05 +0400

    31130.doors.card.filter create mock.json create doors from json edit current and all doors count make filter by collection and  style of door

--- src/components/Card/script.js
+++ src/components/Card/script.js
@@ -1,21 +1,77 @@
+import { formData} from '../Aside/script';
+import doors from './doors.json';
+
 const showMore = document.querySelector('.button--more');
-const doorLength = document.querySelectorAll('.door').length;
-const cardPagination = document.querySelector('.card__pagination');
-let items = 11;
+const itemsToShow = 8;
+const itemsToAdd = 4;
+let activeItems = 0;
+let filtered = [];
+let isFiltered = false; 
+const template = document.querySelector('#card__template').content;
+const container = document.querySelector('.card__inner');
+const quantityCurrent = document.querySelector('.card__quantity-current');
+const quantityMax = document.querySelector('.card__quantity-total');
+const form = document.querySelector('.aside__filter');
 
 showMore.addEventListener('click', () => {
-	items += 4;
-	const array = Array.from(document.querySelector('.card__inner').children);
-	const visItems = array.slice(0, items);
-
-	visItems.forEach(el => el.classList.add('visible'));
-
-	if (visItems.length === doorLength) {
-			cardPagination.style.display = 'none';
-
-			const cardInners = document.querySelectorAll('.card__inner');
-    		cardInners.forEach(cardInner => {
-        			cardInner.style.margin = '0';
-    		});
-	}
+    renderList(doors, itemsToAdd);
+    if (quantityCurrent.textContent === quantityMax.textContent) {
+        showMore.style.display = 'none';
+    }
 });
+
+function renderList(list, count, filter) {
+    if (filter) {
+        for (let i = 0; i < container.children.length; i++) {
+            if (container.children[i].classList.contains('door')) {
+                container.removeChild(container.children[i]);
+                i--;
+            }
+        }
+        activeItems = 0;
+        isFiltered = true;
+    } else {
+        isFiltered = false;
+    }
+
+    const activeCount = activeItems + 1 +  count <=  list.length ? count : list.length - activeItems;
+    for (let i = 0; i < activeCount; i++) {
+        const door = template.cloneNode(true);
+
+        if ((i === 0 || i === 1) && activeItems === 0) {
+            door.querySelector('.door').classList.add('door--intro');
+        }
+
+        const picture = door.querySelector('.door__image-box');
+        picture.querySelector('source').srcset = list[i].imageWebp;
+        picture.querySelector('img').src = list[i].imageJpg;
+        const mark = door.querySelector('.door__mark'); 
+        mark.classList.add(list[i].markClass);
+        mark.textContent = list[i].mark;
+        door.querySelector('.door__title').textContent = list[i].title;
+        door.querySelector('.door__price-new').textContent = list[i].priceNew;
+        door.querySelector('.door__price-old').textContent = list[i].priceOld;
+        container.appendChild(door);
+    }
+
+    activeItems += activeCount;
+    quantityCurrent.textContent = activeItems;
+    quantityMax.textContent = filter ? filtered.length : doors.length;
+};
+
+function checkFields (item) {
+    let active = true;
+    for (let [name, value] of formData) {
+        if (value && item[name].toLowerCase() !== value) {
+            active = false;
+        } 
+    }
+    return active;
+}
+
+form.addEventListener('edit', () => {
+    filtered = doors.filter((door) => checkFields(door));
+    renderList(filtered, itemsToShow, true);
+});
+
+renderList(doors, itemsToShow);
