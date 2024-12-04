ce4a88744511b053e8c3f2af37633f65b5ec5e74
Yuhlimov Mikhail <mthat@vk.com>
2024-12-04 12:19:31 +0400

    31142.map server get settings

--- src/components/Map/script.js
+++ src/components/Map/script.js
@@ -3,7 +3,7 @@ async function initMap() {
     // Промис `ymaps3.ready` будет зарезолвлен, когда загрузятся все компоненты основного модуля API
     await ymaps3.ready;
 
-    const {YMap, YMapDefaultSchemeLayer} = ymaps3;
+    const { YMap, YMapDefaultSchemeLayer, YMapDefaultFeaturesLayer, YMapMarker } = ymaps3;
 
     // Иницилиазируем карту
     const map = new YMap(
@@ -22,6 +22,63 @@ async function initMap() {
         }
     );
 
+    map.addChild(new YMapDefaultFeaturesLayer());
+
     // Добавляем слой для отображения схематической карты
-    map.addChild(new YMapDefaultSchemeLayer());
+    map.addChild(new YMapDefaultSchemeLayer({
+        customization: [
+            {
+                tags: {
+                    any: ['road']
+                },
+                elements: 'geometry',
+                stylers: [
+                    {
+                        color: '#ffffff'
+                    }
+                ]
+            },
+            {
+                tags: {
+                    all: ['water']
+                },
+                elements: 'geometry',
+                stylers: [
+                    {
+                        color: '#999999'
+                    }
+                ]
+            },
+            {
+                types: 'polygon',
+                tags: {
+                    any: ['admin', 'urban_area', 'poi', 'landscape']
+                },
+                stylers: [
+                    {
+                        color: '#f0f0f0'
+                    }
+                ]
+            }
+        ]
+    }));
+
+
+    const content = document.createElement('section');
+
+    // Инициализируйте маркер
+    const marker = new YMapMarker(
+      {
+        coordinates: [44.006516, 56.322797],
+        draggable: true
+      },
+      content
+    );
+    
+    // Добавьте маркер на карту
+    map.addChild(marker);
+    
+    // Добавьте произвольную HTML-разметку внутрь содержимого маркера
+    content.innerHTML = '<h1>Этот заголовок можно перетаскивать</h1>';
+    // style=tags.any:road|elements:geometry|stylers.color:ffffff~tags.all:water|elements:geometry|stylers.color:999999~types:polygon|tags.any:admin;urban_area;poi;landscape|stylers.color:f0f0f0
 }
