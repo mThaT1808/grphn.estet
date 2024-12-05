da8c7cc53e3b97d52bb416d9d8818e2a619b61b3
Артём <prubv@yandex.ru>
2024-12-05 17:54:30 +0400

    31130.doors.cart.filter finish filter add checked status add more colors

--- src/components/Aside/script.js
+++ src/components/Aside/script.js
@@ -44,8 +44,31 @@ const filterLists = document.querySelectorAll('.filter__list');
 filterLists.forEach((list) => {
     list.addEventListener('click', (e) => {
         if (e.target.classList.contains('filter__item')) {
-            list.dataset.value = e.target.dataset.value;
+            e.target.classList.toggle('filter__item--active');
+            if (formData.has(list.dataset.name)) {
+                if(list.dataset.value.includes(e.target.dataset.value)) {
+                    console.log(e.target.dataset.value);
+                    const text = formData.get(list.dataset.name).replace(e.target.dataset.value, '');
+                    list.dataset.value = text;
+                    if (list.dataset.value.trim() === '') {
+                        list.dataset.value = list.dataset.value.trim();
+                        console.log(`'${list.dataset.value.trim()}'`);
+                    }
+                    collectData();
+                    return;
+                }            
+            }
+           
+            if (list.dataset.value) {
+                list.dataset.value += ` ${e.target.dataset.value}`;
+                console.log('+');
+            } else {
+                list.dataset.value = e.target.dataset.value;
+                console.log('new');
+            }
             collectData();
+            console.log(formData);
+            console.log(list.dataset.value);
         }
     });
 });
@@ -55,6 +78,14 @@ const editEvent = new CustomEvent("edit", {bubbles : true, cancelable : true, de
 let formData = new FormData(form);
 function collectData () {
     formData = new FormData(form);
+    const colors = [];
+    for (let [name, value] of formData) {
+        if (name === 'color') {
+            colors.push(value);
+        }
+    }
+    formData.append('colors', colors);
+    formData.delete('color');
     filterLists.forEach((list) => {
         if (!list.classList.contains('filter__list-input')) {
             formData.append(list.dataset.name, list.dataset.value); 
@@ -68,7 +99,10 @@ function reset() {
     formData = new FormData();
     filterLists.forEach((list) => {
         list.dataset.value = '';
+        const items = list.querySelectorAll('.filter__item[data-value]');
+        items.forEach((item) => item.classList.remove('filter__item--active'));
     });
+
 }
 
 
--- src/components/Card/script.js
+++ src/components/Card/script.js
@@ -1,5 +1,5 @@
 import { formData} from '../Aside/script';
-import doors from './doors.json';
+import {getData} from './server.js';
 
 const showMore = document.querySelector('.button--more');
 const itemsToShow = 8;
@@ -12,15 +12,33 @@ const container = document.querySelector('.card__inner');
 const quantityCurrent = document.querySelector('.card__quantity-current');
 const quantityMax = document.querySelector('.card__quantity-total');
 const form = document.querySelector('.aside__filter');
+const doors = await getData();
+const pagination = document.querySelector('.card__quantity');
 
-showMore.addEventListener('click', () => {
-    renderList(doors, itemsToAdd);
+function checkQuantity() {
+    if (quantityCurrent.textContent === '0') {
+        pagination.style.display = 'none';
+    } else {
+        pagination.style.display = 'block';
+    }
+
     if (quantityCurrent.textContent === quantityMax.textContent) {
         showMore.style.display = 'none';
+    } else {
+        showMore.style.display = 'block';
     }
+}
+
+showMore.addEventListener('click', () => {
+    if (isFiltered) {
+        renderList(filtered, itemsToAdd, false, true);
+    } else {
+        renderList(doors, itemsToAdd);
+    }
+    checkQuantity();
 });
 
-function renderList(list, count, filter) {
+function renderList(list, count, filter, add) {
     if (filter) {
         for (let i = 0; i < container.children.length; i++) {
             if (container.children[i].classList.contains('door')) {
@@ -30,7 +48,7 @@ function renderList(list, count, filter) {
         }
         activeItems = 0;
         isFiltered = true;
-    } else {
+    } if (!filter && !add) {
         isFiltered = false;
     }
 
@@ -46,25 +64,87 @@ function renderList(list, count, filter) {
         picture.querySelector('source').srcset = list[i].imageWebp;
         picture.querySelector('img').src = list[i].imageJpg;
         const mark = door.querySelector('.door__mark'); 
-        mark.classList.add(list[i].markClass);
+        mark.classList.add(`door__mark--${list[i].markClass}`);
         mark.textContent = list[i].mark;
         door.querySelector('.door__title').textContent = list[i].title;
-        door.querySelector('.door__price-new').textContent = list[i].priceNew;
+        door.querySelector('.door__price-new').textContent = `от ${list[i].priceNew}\u20BD`;
         door.querySelector('.door__price-old').textContent = list[i].priceOld;
+        const colorContainer = door.querySelector('.door__choice-box');
+        for (let j = 0; j < list[i].colors.length; j++) {
+            const button = colorContainer.querySelector('.door__choice-btn');
+            if (j >= 5 && document.documentElement.clientWidth <= 1660) {
+               button.textContent = `+${list[i].colors.length - j}`;    
+               break;
+            }
+
+            if (list[i].colors.length - 5 <= 0) {
+                button.style.display = 'none';
+            }
+
+            const item = list[i].colors[j];
+            const label = document.createElement('label');
+            label.key = item;
+            const img = document.createElement('img');
+            img.classList.add('door__choice-textures');
+            img.src = `/images/card/texture/${item}.png`;
+            img.alt=""
+            const input = document.createElement('input');
+            input.classList.add('door__choice');
+            input.type = 'radio';
+            input.name = `card-color`;
+            input.value = item;
+            label.appendChild(img);
+            label.appendChild(input);
+            colorContainer.insertBefore(label, button);
+        };
         container.appendChild(door);
     }
 
     activeItems += activeCount;
     quantityCurrent.textContent = activeItems;
-    quantityMax.textContent = filter ? filtered.length : doors.length;
+    quantityMax.textContent = filter || add ? filtered.length : doors.length;
 };
 
 function checkFields (item) {
     let active = true;
     for (let [name, value] of formData) {
-        if (value && item[name].toLowerCase() !== value) {
+        if (name === 'discount' && value === 'on' && item.markClass !== 'promo') {
             active = false;
-        } 
+        }
+        
+        if (name === 'colors') {
+            if (value) {
+                value.split(',').forEach((val) => {
+                    if (!item.colors.includes(val)) {
+                        active = false;
+                    }
+                });
+            }
+        }
+
+        if (name === 'style') {
+            if (value) {
+                if (!value.split(' ').includes(item.style.toLowerCase())) {
+                    active = false;
+                }   
+            }
+        }
+
+        if (name === 'collection') {
+            if (value) {
+                if (!value.split(' ').includes(item.collection.toLowerCase())) {
+                    active = false;
+                }   
+            }
+        }
+
+        if (name === 'finishing') {
+            if (value) {
+                if (!value.split(' ').includes(item.finishing.toLowerCase())) {
+                    active = false;
+                }   
+            }
+        }
     }
     return active;
 }
@@ -72,6 +152,7 @@ function checkFields (item) {
 form.addEventListener('edit', () => {
     filtered = doors.filter((door) => checkFields(door));
     renderList(filtered, itemsToShow, true);
+    checkQuantity();
 });
 
 renderList(doors, itemsToShow);
