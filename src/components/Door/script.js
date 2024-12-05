5f28bc551b4c8125f8b5b6e8cf17d83a945a68a7
Dasha Savelyeva <ssavellievaa@yandex.ru>
2024-12-05 17:55:22 +0400

    31094.doors.card.815 click to heart

---
+++ src/components/Door/script.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('.door__mark-like').forEach(function(container) {
+    container.addEventListener('click', function() {
+        const icon = this.querySelector('i');
+    
+        icon.classList.toggle('icon-heart');
+        icon.classList.toggle('icon-heart-fill');
+    })
+});
