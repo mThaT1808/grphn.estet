7174076f6c9bbb75e68f1c60ee02ca062b51b0fc
Dasha Savelyeva <ssavellievaa@yandex.ru>
2024-12-06 11:51:35 +0400

    31094.doors.card.815 count for favorite

--- src/components/Door/script.js
+++ src/components/Door/script.js
@@ -1,10 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
+    let count = 0;
+    const quantityElement = document.querySelector('.personal__quantity');
+
     document.querySelectorAll('.door__mark-like').forEach(function(container) {
         container.addEventListener('click', function() {
             const icon = this.querySelector('i');
-
             icon.classList.toggle('icon-heart');
             icon.classList.toggle('icon-heart-fill');
+
+            if (icon.classList.contains('icon-heart-fill')) {
+                count++;
+            } else {
+                count--;
+            }
+
+            quantityElement.textContent = count;
+
+            if (count > 0) {
+                quantityElement.style.display = 'block';
+            } else {
+                quantityElement.style.display = 'none';
+            }
         });
     });
 });
